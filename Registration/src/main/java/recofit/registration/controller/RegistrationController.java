f6796fd027996802be38bfd01a2365e2849c8fa5
yanwenri <yanwenri@hyuperiun>
2022-08-15 23:45:12 +0900

    fourth commit

---
+++ Registration/src/main/java/recofit/registration/controller/RegistrationController.java
@@ -0,0 +1,45 @@
+package recofit.registration.controller;
+
+//import recofit.registration.dto.RegGymDto;
+//import recofit.registration.dto.RegPtProgramDto;
+import recofit.registration.dto.RegistrationDto;
+//import recofit.registration.kafka.Producer;
+import recofit.registration.message.ResponseMessage;
+import recofit.registration.service.RegistrationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+import java.util.List;
+
+@RestController
+@RequestMapping("/registration")
+public class RegistrationController {
+	
+    @Autowired
+    public RegistrationService regService;
+
+//    @Autowired
+//    public Producer producer;
+/*
+    @PostMapping("/new")
+    public ResponseEntity<ResponseMessage> saveNewRegistration(@RequestBody RegistrationDto regDto){
+        regService.saveNewRegistration(regDto);
+
+        ResponseMessage responseMessage = ResponseMessage.builder()
+                .message("save Registration Information Success.")
+                .responseTime(new Date())
+                .data(null)
+                .build();
+
+        return new ResponseEntity<ResponseMessage>(responseMessage, HttpStatus.OK);
+    }
+*/
+}
